fcf2c98b48d2a0d3c97d1d0e57b66a9780e89bbc
atomic-gong <oliver.alcaraz@thedropbears.org.au>
2022-06-27 14:26:37 +1000

    Created mod installer

---
+++ ModDownloader.ps1
@@ -0,0 +1,15 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+$InstallDir='C:\ProgramData\chocoportable'
+$env:ChocolateyInstall="$InstallDir"
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco upgrade chocolatey -y
+choco install git -y
+cmd.exe /c git clone https://github.com/atomic-gong/dynamic-world.git
